462709f6635df18efde8184bf7b441609b031220
Michael Laryea <80675696+micky-4u@users.noreply.github.com>
2021-05-19 21:11:14 +0000

    Update main.py

--- main.py
+++ main.py
@@ -3,17 +3,6 @@
 # getting words from the user
 word1, word2= input("Enter two words (seperate by space): ").split()
 
-# this loop delets all duplicate characters in words
-for i in word1:
-    if word1.count(i) > 1:
-      word1=  i+word1.replace(i, '')
-
-for i in word2:
-    if word2.count(i) > 1:
-       word2= i+word2.replace(i, '')
-        
-print("anagrame characters:"+ word1)
-
 #sorting the words alphabetically and comparing 
 if sorted(word1)==sorted(word2):
     print("Your words are anagramms")
